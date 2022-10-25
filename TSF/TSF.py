05047007b1527f0c1e27e17c0546ea0934e497d8
AdityaSubrahmanyaBhat <70971391+AdityaSubrahmanyaBhat@users.noreply.github.com>
2022-10-25 16:53:26 +0530

    Modified TSF

--- TSF/TSF.py
+++ TSF/TSF.py
@@ -10,20 +10,21 @@ class TSF:
     def __init__(self) -> None:
         df=pd.read_csv('TSF/temperature.csv',parse_dates=True,index_col="DATE")
         df=df.dropna()
-        self.train=df.iloc[:-30]
+        self.train_data=df.iloc[:-30]
         self.test=df.iloc[-30:]
 
     def train(self)->None:
-        self.model=SARIMAX(self.train["AvgTemp"],order=(1,0,5))
+        self.model=SARIMAX(self.train_data["AvgTemp"],order=(1,0,5))
         self.model=self.model.fit()
 
-    def predict(self)->None:
-        start=len(self.train)
-        end=len(self.train)+len(self.test)-1
-        self.pred=self.model.predict(start=start,end=end,typ='levels').rename('ARIMA Predictions')
+    def predict_(self)->None:
+        start=len(self.train_data)
+        end=len(self.train_data)+len(self.test)-1
+        self.pred=self.model.predict(start=start,end=end,typ='levels')
 
     def metrics(self)->None:
-        self.MAE = metrics.mean_absolute_error(self.test, self.pred)
+        self.test=self.test.drop(["MinTemp","MaxTemp","Sunset","Sunrise"],axis=1)
+        self.MAE = metrics.mean_absolute_error(self.test,self.pred)
         self.MSE = metrics.mean_squared_error(self.test, self.pred)
         self.RMSE = np.sqrt(self.MSE)
         print("MAE = "+str(self.MAE))
@@ -33,5 +34,5 @@ class TSF:
 if __name__=="__main__":
     tsf=TSF()
     tsf.train()
-    tsf.predict()
+    tsf.predict_()
     tsf.metrics()
